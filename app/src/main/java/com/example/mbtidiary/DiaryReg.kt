40c58f41672ff34ae56f6c838b7b00338c060a83
1songe <songe@swu.ac.kr>
2023-01-20 23:04:02 +0900

    Layout

---
+++ app/src/main/java/com/example/mbtidiary/DiaryReg.kt
@@ -0,0 +1,11 @@
+package com.example.mbtidiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiaryReg : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_reg)
+    }
+}
