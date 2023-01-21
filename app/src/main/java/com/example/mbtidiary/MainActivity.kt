3bcfe6aec193b40740126c68a0a03192c33476a3
1songe <songe@swu.ac.kr>
2023-01-22 00:08:33 +0900

    MainActivity button connection

--- app/src/main/java/com/example/mbtidiary/MainActivity.kt
+++ app/src/main/java/com/example/mbtidiary/MainActivity.kt
@@ -5,11 +5,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnTest: Button
+    lateinit var btnList: ImageButton
+    lateinit var btnStat: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnTest = findViewById<Button>(R.id.btnTest)
+        btnList = findViewById<ImageButton>(R.id.btnList)
+        btnStat = findViewById<ImageButton>(R.id.btnStat)
+
+        btnTest.setOnClickListener {
+            var intent = Intent(this, MBTITest::class.java)
+            startActivity(intent)
+        }
+        btnList.setOnClickListener {
+            var intent = Intent(this, DiaryList::class.java)
+            startActivity(intent)
+        }
+        btnStat.setOnClickListener {
+            var intent = Intent(this, MBTIStatics::class.java)
+            startActivity(intent)
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
